67d2e2b43f346eb5b5bf1a4f21b6501ad9c8f06c
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-14 18:45:39 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const populationList = Array.from(document.querySelectorAll('.population')).map(
+  (node) => node.textContent,
+);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = populationList.reduce(
+  (pref, number) => pref + convertToNumber(number),
+  0,
+);
+const average = total / populationList.length;
+
+totalPopulation.textContent = total;
+averagePopulation.textContent = average;
+
+function convertToNumber(string) {
+  let result = '';
+
+  for (const ch of string) {
+    if (ch === ',') {
+      continue;
+    }
+
+    result += ch;
+  }
+
+  return Number(result);
+}
